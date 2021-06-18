2aebd3c66a34675968b62ff1f0f95b5d08b967b8
madhav23bansal <madhavb230100@gmail.com>
2021-06-18 13:49:10 +0530

    Added githubrepofetch function in main.dart api-sdk

--- api_sdk/lib/main.dart
+++ api_sdk/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:api_sdk/api_constants.dart';
+import 'package:api_sdk/graphql_method/graphql_handler.dart';
 import 'package:api_sdk/rest/rest_api_handler_data.dart';
 
 class ApiSdk {
@@ -17,6 +18,13 @@ class ApiSdk {
   static getUserData(int id) async {
     final response =
         await RestApiHandlerData.getData('${apiConstants["auth"]}/users/$id');
+    return response;
+  }
+
+  static fetchGithubRepoGraphQl(numOfRepositories) async {
+    final GraphqlQlHandler githubRepository =
+        GraphqlQlHandler(client: client());
+    final response = await githubRepository.getRepositories(numOfRepositories);
     return response;
   }
 }
