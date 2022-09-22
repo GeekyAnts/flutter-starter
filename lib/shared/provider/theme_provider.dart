abcdb17127a029e0399b2ef73e8a0f236ad95847
Devanshi Garg <devanshig@geekyants.com>
2022-09-22 12:39:27 +0530

    📝 coments added

--- lib/shared/provider/theme_provider.dart
+++ lib/shared/provider/theme_provider.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
-
 import '../../flutter_starter.dart';
 
 enum AppTheme { light, dark }
 
+/// ThemeProvider is responsible for updating the app theme
+/// from light to dark and vice versa.
 class ThemeProvider extends BaseProvider {
   AppTheme appTheme = AppTheme.light;
   void setTheme(AppTheme theme) {
